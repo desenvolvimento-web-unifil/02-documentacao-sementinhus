d1b37968c80e728479e7721a6a1f8741b66afa18
Cecilia Pessoal <mariacecilia@edu.unifil.br>
2023-06-20 18:19:53 -0300

    att dash

--- sementinhus-bet/resources/views/dashboard.blade.php
+++ sementinhus-bet/resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
@@ -14,5 +15,35 @@
             </div>
         </div>
     </div>
+
+    <div class="mt-12">
+    <div class="max-w-7xl mx-auto flex sm:px-6 lg:px-8">
+        <div class="w-1/4">
+            <div class="bg-white dark:bg-gray-800 rounded-lg shadow-lg">
+                <div class="p-6 space-y-4">
+                    <h2 class="text-2xl font-semibold text-gray-900 dark:text-white mb-4">Tipos de Jogos</h2>
+                    <ul class="space-y-2">
+                        <li>
+                            <a href="#" class="text-gray-700 dark:text-gray-300 hover:text-red-500">Apostas Esportivas</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-700 dark:text-gray-300 hover:text-red-500">Caça-Níqueis</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-700 dark:text-gray-300 hover:text-red-500">Poker</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-700 dark:text-gray-300 hover:text-red-500">Roleta</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-700 dark:text-gray-300 hover:text-red-500">Bingo</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 </x-app-layout>
